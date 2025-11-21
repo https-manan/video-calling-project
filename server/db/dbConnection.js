ed35ce55f2e0ef1f11ac6b4f04d63ea5490eb822
Manan <Mananbhardwaj2705@gmail.com>
2025-11-21 13:02:45 +0530

    Added user Route and controller.

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect("mongodb+srv://mananbhardwaj2705:manan27bhardwaj@cluster0.ttqvo.mongodb.net/video-calling")
+        await mongoose.connect(process.env.DB_URL)
         console.log('Connected to DB')
     } catch (error) {
         console.log(error)
--- server/index.js
+++ server/index.js
@@ -7,12 +7,15 @@ import cors from 'cors'
 dotenv.config()
 const PORT = process.env.PORT
 import cookieParser from 'cookie-parser';
+import route from './routes/userRoute.js';
 
 
 app.use(cookieParser());
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth',Route)
+app.use('/api/user',route)
+
 dbConnection();
 
 app.listen(PORT,()=>{
