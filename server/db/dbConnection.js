1c92c7aaba6e3a6e82af92de5cfc84384e9b47bb
Manan <Mananbhardwaj2705@gmail.com>
2025-11-27 00:18:34 +0530

    Added the full backend code file. COMPLETED

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,13 +1,6 @@
 import mongoose from "mongoose";
 
-const dbConnection = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL)
-        console.log('Connected to DB')
-    } catch (error) {
-        console.log(error)
-    }
+export const DbConnection = async ()=>{
+    await mongoose.connect(process.env.DB_URL)
+    console.log('Conneted to DB')
 }
-
-export default dbConnection;
- 
--- server/index.js
+++ server/index.js
@@ -1,23 +1,59 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import dbConnection from "./db/dbConnection.js";
-import Route from './routes/authRuote.js';
-const app = express()
-import cors from 'cors'
-dotenv.config()
-const PORT = process.env.PORT
-import cookieParser from 'cookie-parser';
-import route from './routes/userRoute.js';
+import { Server } from "socket.io";
+import env from 'dotenv';
+
+const io = new Server(3000, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: false
+    }
+});
+
+import { DbConnection } from "./Db/dbConnection.js";
+env.config();
+
+DbConnection();
+
+const emailToSocketMap = new Map()
+const socketIdtoEmailMap = new Map()
+io.on('connection', (socket) => {
+    console.log(`Socket connected ${socket.id}`)
+
+    socket.on('room:join', (data) => {
+        console.log(data)
+        const { email, roomId } = data;
+        emailToSocketMap.set(email, socket.id)
+        socketIdtoEmailMap.set(socket.id, email);
+        io.to(socket.id).emit('room:join', data); //Okey so when we get socket.emit('room:join') from Lobby then 
+        //“Send an event called room:join to that specific client, with this data.”
+
+        io.to(roomId).emit("user:joined", { email, id: socket.id })
+        // so basically jo user have this roomId uske pass ye emit ka message chla jayga 
+        // aand "user:joined" pauch jayga
+
+        socket.join(roomId) //Then jouser hoga with that roomId usko hum join kra denge
+        
+    })
+
+    socket.on('user:call', ({ to, offer }) => {
+        io.to(to).emit('incoming:call', { from: socket.id, offer }) 
+        //io.to(to) so here this(to) means the rmeoteId 
+        //kykoi hum to:remoteId vala user bhaj rhe h
+    })
+
+    socket.on('call:accepted', ({ to, ans }) => {
+        io.to(to).emit('call:accepted', { from: socket.id, ans })
+    })
 
 
-app.use(cookieParser());
-app.use(cors())
-app.use(express.json())
-app.use('/api/auth',Route)
-app.use('/api/user',route)
+    socket.on('peer:nego:needed', ({ offer, to }) => {
+        //When negotiationneeded fires on one peer, send the new offer to the other peer
+        io.to(to).emit('peer:nego:needed', { from: socket.id, offer })
+    })
 
-dbConnection();
-
-app.listen(PORT,()=>{
-    console.log(`Listening on port : ${PORT}`)
+    socket.on('peer:nego:done', ({ to, ans }) => {
+        //Second peer sends back answer (ans) to complete negotiation
+        io.to(to).emit('peer:nego:final', { from: socket.id, ans })
+    })
+    // ------------------------------------------------------------------
 })
